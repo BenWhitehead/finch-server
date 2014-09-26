b558b5cda73ebc2ce286a16df7cbebb7d09b5d3d
BenWhitehead <benjamin.r.whitehead@gmail.com>
2014-09-25 21:06:35 -0700

    Add request readers for String, JSONObject and JSONArray.

--- src/main/scala/io/github/benwhitehead/finch/package.scala
+++ src/main/scala/io/github/benwhitehead/finch/package.scala
@@ -15,19 +15,15 @@
 
 package io.github.benwhitehead
 
-import com.twitter.app.{App => TApp}
+import com.twitter.finagle.Service
 import com.twitter.finagle.http.Response
-import com.twitter.finagle.{Service, SimpleFilter}
-import com.twitter.server.Stats
 import com.twitter.util.Future
 import io.finch._
-import io.finch.json.JsonObject
-import io.finch.request.{ParamNotFound, ValidationFailed}
-import io.finch.response._
+import io.finch.request.RequestReader
 import org.jboss.netty.handler.codec.http.HttpResponseStatus
+import org.jboss.netty.util.CharsetUtil
 
-import java.text.SimpleDateFormat
-import java.util.Date
+import scala.util.parsing.json.{JSON, JSONArray, JSONObject}
 
 package object finch {
 
@@ -54,6 +50,92 @@ package object finch {
       rep.setContentTypeJson()
       rep.setContentString(JacksonWrapper.serialize(request))
       rep.toFuture
+    }
+  }
+
+  object RequiredStringBody {
+    def apply() = new RequestReader[String] {
+      def apply(req: HttpRequest): Future[String] = {
+        req.contentLength match {
+          case Some(length) if length > 0 => req.content.toString(CharsetUtil.UTF_8).toFuture
+          case _                          => new BadRequest().toFutureException
+        }
+      }
+    }
+  }
+
+  object OptionalStringBody {
+    def apply() = new RequestReader[Option[String]] {
+      def apply(req: HttpRequest): Future[Option[String]] = {
+        req.contentLength match {
+          case Some(length) if length > 0 => Some(req.content.toString(CharsetUtil.UTF_8)).toFuture
+          case _                          => None.toFuture
+        }
+      }
+    }
+  }
+
+  object RequiredJSONObjectBody {
+    def apply() = new RequestReader[JSONObject] {
+      def apply(req: HttpRequest): Future[JSONObject] = {
+        req.headerMap.get("Content-Type") match {
+          case Some("application/json") =>
+            JSON.parseRaw(req.content.toString(CharsetUtil.UTF_8)) match {
+              case Some(obj: JSONObject) => obj.toFuture
+              case Some(arr: JSONArray) => new BadRequest().toFutureException
+              case None => new BadRequest().toFutureException
+            }
+          case _ => new BadRequest().toFutureException
+        }
+      }
+    }
+  }
+
+  object RequiredJSONArrayBody {
+    def apply() = new RequestReader[JSONArray] {
+      def apply(req: HttpRequest): Future[JSONArray] = {
+        req.headerMap.get("Content-Type") match {
+          case Some("application/json") =>
+            JSON.parseRaw(req.content.toString(CharsetUtil.UTF_8)) match {
+              case Some(obj: JSONObject) => new BadRequest().toFutureException
+              case Some(arr: JSONArray) => arr.toFuture
+              case None => new BadRequest().toFutureException
+            }
+          case _ => new BadRequest().toFutureException
+        }
+      }
+    }
+  }
+
+  object OptionalJSONObjectBody {
+    def apply() = new RequestReader[Option[JSONObject]] {
+      def apply(req: HttpRequest): Future[Option[JSONObject]] = {
+        req.headerMap.get("Content-Type") match {
+          case Some("application/json") =>
+            JSON.parseRaw(req.content.toString(CharsetUtil.UTF_8)) match {
+              case Some(obj: JSONObject) => Some(obj).toFuture
+              case Some(arr: JSONArray) => None.toFuture
+              case None => None.toFuture
+            }
+          case _ => None.toFuture
+        }
+      }
+    }
+  }
+
+  object OptionalJSONArrayBody {
+    def apply() = new RequestReader[Option[JSONArray]] {
+      def apply(req: HttpRequest): Future[Option[JSONArray]] = {
+        req.headerMap.get("Content-Type") match {
+          case Some("application/json") =>
+            JSON.parseRaw(req.content.toString(CharsetUtil.UTF_8)) match {
+              case Some(obj: JSONObject) => None.toFuture
+              case Some(arr: JSONArray) => Some(arr).toFuture
+              case None => None.toFuture
+            }
+          case _ => None.toFuture
+        }
+      }
     }
   }
 
